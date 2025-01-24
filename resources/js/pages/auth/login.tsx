9a09826ec3c990021ce0371c2d338c61b766ed59
arismetyogi <arismetyogi@gmail.com>
2025-01-24 11:49:39 +0700

    add csrf to axios bootstrap

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -12,7 +12,6 @@ import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {InputError} from "@/components/ui/input-error";
-import axiosInstance from "@/lib/axios";
 import axios from "axios";
 
 export default function Login({
